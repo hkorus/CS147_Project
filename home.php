daa0e3656b8984a7fc93b26e42b3cf33222a2558
Lynn Cuthriell <lcuthriell@gmail.com>
2012-10-28 13:35:01 -0700

    Added home.php

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+	<title>Home Page</title>
+	<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+ 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+	<link rel="stylesheet" href="jquery.mobile-1.2.0.css" />
+	<link rel="stylesheet" href="style.css" />
+	<link rel="apple-touch-icon" href="appicon.png" />
+	<link rel="apple-touch-startup-image" href="startup.png">
+	
+	<script src="jquery-1.8.2.min.js"></script>
+	<script src="jquery.mobile-1.2.0.js"></script>
+</head>
+<body>
+	
+	<div data-role="header">
+		<h1>Art</h1>
+	</div>
+	<p>HOMIE PAGE</p>
+
+	<p>Gimme dat <a href = "./art.html"> art</a>!</p>
+	<p>I already found art I like. Take me to the  <a href = "./favorites.html"> favorites</a>! </p>
+
+	<p>I want some <a href = "./help.html"> help</a></p>
+
+</body>
+
+</html>
