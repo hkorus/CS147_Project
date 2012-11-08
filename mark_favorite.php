416f0120e0e946ebae4ef6ec7ac03ca6d9c44cac
Hannah Korus <hkorus@gmail.com>
2012-11-08 00:19:19 -0800

    saving comments works again. can add favorites to fb user

--- mark_favorite.php
+++ mark_favorite.php
@@ -1,8 +1,16 @@
 <?php
 include("config.php");
-
+	require './facebook.php';
+	$facebook = new Facebook(array(
+		'appId'  => '291103611004949',
+  		'secret' => '226db60e672abf202f1424b1084fc38e',
+      	'cookie' => true));
+      	
+    $fb_user = $facebook->getUser();
+    
 	$imageData=$GLOBALS['HTTP_RAW_POST_DATA'];
-	$query = "INSERT INTO fave_art VALUES (1,".$imageData.",NULL);";
+	//$query = "INSERT INTO fave_art VALUES (1,".$imageData.",NULL);";
+	$query = "INSERT INTO fave_art VALUES (".$fb_user.",".$imageData.",NULL);";
 	$result = mysql_query($query);
 
 	
